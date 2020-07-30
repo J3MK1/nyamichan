dba5cbc7f2397aca4757469cebb710d5eb352132
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2020-07-30 19:31:37 -0300

    Adding files

---
+++ commands/aboutme.js
@@ -0,0 +1,7 @@
+module.exports ={
+    name: 'aboutme',
+    description: 'Get Mei Lin Draws Informations',
+    execute(message, args){
+        message.channel.send('Hello World!');
+    }
+}
---
+++ commands/email.js
@@ -0,0 +1,7 @@
+module.exports ={
+    name: 'email',
+    description: "E-mail oficial de contato da Academia Reiwa.",
+    execute(message, args){
+        message.channel.send('*Rei-rei! ♪ ~*\n\n**Este é o nosso endereço de e-mail:** reiwacademy@gmail.com');
+    }
+}
