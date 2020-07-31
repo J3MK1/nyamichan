b8a57afdc05f8e56ba2ab038fa61972d89b4fe24
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2020-07-30 23:27:01 -0300

    Fixing Files

--- commands/commission.js
+++ commands/commission.js
@@ -1,5 +1,5 @@
 module.exports ={
-    name: 'commissions',
+    name: 'commission',
     description: "Commissions of MeiLin Draws.",
     execute(message, args){
         message.channel.send('*My master left this message to you:* \n\n "I was going to open in the early July 2020 but due to my recent open project (Patreon) having to postpone and for **I dont know exactly when yet**, I will be receiving only a few orders for people who had already asked for a slot months ago, I hope you understand and Im sorry for the unforeseen, I dont like working rushing and make a low-quality job. \n\n For the **Character Design**. I will also be **temporarily closing** to avoid waiting 2+ months on the part of you, as client, as I know it is annoying to wait so long to receive the character so expected.\n\n If you want to know more about check this link:http://fav.me/ddtxkh8"');
--- commands/question.js
+++ commands/question.js
@@ -2,6 +2,6 @@ module.exports ={
     name: 'question',
     description: "FAQ of MeiLin Draws.",
     execute(message, args){
-        message.channel.send('*So you want to know my master`s secrets? I dont know! If I knew I would be called Mei Lin Draws!*\n\n*Welp... she left me a scroll but it is written in a language I dont understand, maybe it can help you*, **here it is: http://fav.me/ddtxkhc !**');
+        message.channel.send('*So you want to know my master`s secrets? I dont know! If I knew I would be called Mei Lin Draws!*\n\n*Welp... she left me a scroll but it is written in a language I dont understand, maybe it can help you*, **here it is:** http://fav.me/ddtxkhc !');
     }
 }
