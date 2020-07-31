32d8034513fab0a0807f3d0d877c78c9583475b9
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2020-07-31 02:53:27 -0300

    Fixing file

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const prefix = '!';
 
 const fs = require('fs');
 
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
 const channelId = '738567304065056859' // welcome Id
 
 const discussionId = '716316265823600730' // discussion Id
@@ -18,12 +22,6 @@ for(const file of commandFiles){
  
     client.commands.set(command.name, command);
 }
-
-var numbers = new Array(10)
-for (var i = 0; i < numbers.length; i++) {
-  numbers[i] = randomIntInc(1, 10)
-  return numbers;
-}
  
 client.on('guildMemberAdd', member =>{
 
@@ -32,6 +30,51 @@ client.on('guildMemberAdd', member =>{
     const channel = member.guild.channels.cache.get(channelId)
 
     channel.send(`Heey, welcome to our server ${member}, say hello to everyone in ${member.guild.channels.cache.get(discussionId).toString()}! I hope you have a lot of fun here! Yayy! :D`)
+});
+
+// Configure the array used for random replies
+let replies = ["reply 1", "reply 2", "reply 3"];
+
+// Configure the randomizer that will pick a random integer from 0 to the length of the array; used for array index
+let random = Math.floor(Math.random() * replies.length);
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - ('+ bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with /
+    if (message.substring(0, 1) == '/') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // /Hi
+            case 'test1':
+                bot.sendMessage({
+                    to: channelID,
+                    message: replies[random]
+                });
+        break;
+        // Just add any case commands if you want to..
+         }
+     }
 });
  
 client.once('ready', () => {
