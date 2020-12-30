c6e08dea4055f2261fcc5c71393d406c5fd7d455
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2020-12-29 23:36:08 -0300

    Update roles.js

--- commands/roles.js
+++ commands/roles.js
@@ -5,7 +5,7 @@ exports.run = async(client, message, args) => {
     const embed = new Discord.MessageEmbed()
         .setTitle('Exclusive Roles')
         .setColor('#FFB6C1')
-        .setDescription('Here is the Exclusive Roles of the server: <719289193779691530>')
+        .setDescription('Here is the Exclusive Roles of the server: <@719289193779691530>')
         .setImage()
         .setTimestamp()
         .setThumbnail(avatar)
