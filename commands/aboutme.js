e83ebfa5bb530029e3f83265e4a23f21ed584e2d
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2021-05-12 18:30:07 -0300

    [FIX] Commands

--- commands/aboutme.js
+++ commands/aboutme.js
@@ -5,7 +5,7 @@ exports.run = async(client, message, args) => {
     const embed = new Discord.MessageEmbed()
         .setTitle('About MeiLin Draws')
         .setColor('#FFB6C1')
-        .setDescription('*So you want to know about my master? She left some scrolls with me after she arrived from her journey, maybe this could be useful for you!* \n\n **My Brief Bio:** http://fav.me/ddtpm54 \n **My Art Progress:** http://fav.me/ddtr0kn \n **My Gallery:**https://www.deviantart.com/meilindraws/gallery')
+        .setDescription('*So you want to know about my master? She left some scrolls with me after she arrived from her journey, maybe this could be useful for you!* \n\n **My Brief Bio:** http://fav.me/ddtpm54 \n **My Art Progress:** http://fav.me/ddtr0kn \n **My Gallery: **https://www.deviantart.com/meilindraws/gallery')
         .setImage()
         .setTimestamp()
         .setThumbnail(avatar)
--- commands/social.js
+++ commands/social.js
@@ -5,7 +5,7 @@ exports.run = async(client, message, args) => {
     const embed = new Discord.MessageEmbed()
         .setTitle('Social Media')
         .setColor('#FFB6C1')
-        .setDescription('*It is easy! How you dont know your fool !? I am just kidding, I was imitating Kinatsu she would say something like that hah* \n > MeiLin Draws Social Media: \n\n ● Patreon: https://www.patreon.com/meilindraws  \n ● Deviantart: https://www.deviantart.com/meilindraws \n ● Youtube:https://www.youtube.com/channel/UCP9TfHKQzsIAc9Z9IMyQ6eA/?guided_help_flow=5 \n ● Twitter: https://twitter.com/meilindraws \n ● Instagram: https://www.instagram.com/meilin.draws/ \n ● Facebook: https://www.facebook.com/meilindraws01/ \n ● FurAffinity: https://www.furaffinity.net/user/meilindraws/ \n\n *I think that is all... I wonder why my master has so much social media...?*')
+        .setDescription('*It is easy! How you dont know your fool !? I am just kidding, I was imitating Kinatsu she would say something like that hah* \n > MeiLin Draws Social Media: \n\n ● Patreon: https://www.patreon.com/meilindraws  \n ● Deviantart: https://www.deviantart.com/meilindraws \n ● Youtube: https://www.youtube.com/channel/UCP9TfHKQzsIAc9Z9IMyQ6eA/?guided_help_flow=5 \n ● Twitter: https://twitter.com/meilindraws \n ● Instagram: https://www.instagram.com/meilin.draws/ \n ● Facebook: https://www.facebook.com/meilindraws01/ \n ● FurAffinity: https://www.furaffinity.net/user/meilindraws/ \n\n *I think that is all... I wonder why my master has so much social media...?*')
         .setImage()
         .setTimestamp()
         .setThumbnail(avatar)
